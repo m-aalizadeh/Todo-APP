b3f63b05322087c6b60ffd9b72f8a16fc25c8180
Maryam Aalizadeh <maryam.alizadeh@tecnotree.com>
2023-06-25 22:37:47 +0330

    webpack latest chnage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import LoadableApp from "./LoadableApp";
 import reportWebVitals from "./reportWebVitals";
+import "./sass/app.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <LoadableApp />
   </React.StrictMode>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,44 @@
 const path = require("path");
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const modeConfiguration = (env) => require(`./build-utils/webpack.${env}`)(env);
 
 module.exports = ({ mode } = { mode: "production" }) => {
   console.log(`mode is: ${mode}`);
-  return {
-    mode,
-    entry: "./src/index.js",
-    devServer: {
-      open: true,
-    },
-    output: {
-      publicPath: "/",
-      path: path.resolve(__dirname, "build"),
-      filename: "bundle.js",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jpe?g|png$/,
-          exclude: /node_modules/,
-          use: ["url-loader", "file-loader"],
-        },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
-        },
+  return merge(
+    {
+      mode,
+      entry: "./src/index.js",
+      devServer: {
+        open: true,
+      },
+      output: {
+        publicPath: "/",
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js",
+      },
+      module: {
+        rules: [
+          {
+            test: /\.jpe?g|png$/,
+            exclude: /node_modules/,
+            use: ["url-loader", "file-loader"],
+          },
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+          },
+        ],
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: "./public/index.html",
+        }),
+        new webpack.HotModuleReplacementPlugin(),
       ],
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./public/index.html",
-      }),
-    ],
-  };
+    modeConfiguration(mode)
+  );
 };
