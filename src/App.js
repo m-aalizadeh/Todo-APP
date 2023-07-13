33966b0c348c7855d5ba08e035611e574d5bbc57
m-aalizadeh <52911033+m-aalizadeh@users.noreply.github.com>
2023-07-13 18:23:23 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import TodoList from "./components/TodoList";
-import Login from "./components/user/Login";
+import User from "./components/user/User";
 
 function App() {
+  const history = useNavigate();
   return (
     <Grid item xs={12}>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<User history={history} />} />
         <Route path="/todos" element={<TodoList />} />
       </Routes>
     </Grid>
